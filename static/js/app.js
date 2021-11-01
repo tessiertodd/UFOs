17a324cc4098e86172e52242a3477e1969085bc6
tessiertodd <tessiert@yahoo.com>
2021-10-31 22:48:48 -0400

    final table built

--- static/js/app.js
+++ static/js/app.js
@@ -25,3 +25,29 @@ function buildTable(data)  {
         );
     });
 }
+
+
+// Create a handle click function
+function handleClick() {
+    let date = d3.select("#datetime").property("value");
+    let filteredData = tableData;
+
+    // Check to see if a date was entered and filter the
+    // dta using that date.
+    if (date) {
+        // Apply 'filter' to the table data to only keep the
+        // rows where the 'datetime' value matches the filter value
+        filteredData = filteredData.filter(row => row.datetime === date);
+    }
+
+    // Rebuild the table using the filtered data
+    // @NOTE: If no date was entered, then filteredData will
+    // just be the original tableData.
+    buildTable(filteredData);
+}
+
+// Attach an event to listen for the form button
+d3.selectALL("#filter-btn").on("click", handleClick);
+
+// Build the table when the page loads
+buildTable(tableData);
